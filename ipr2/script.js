697b78d72631271195fa036a2d4b1c59f6255ad8
Pavel Kuzmich <pavel_kuzmich@epam.com>
2020-10-03 21:45:17 +0300

    wip

--- ipr2/script.js
+++ ipr2/script.js
@@ -1,7 +1,39 @@
-var theBiggest = function(a, b) {
-    var result;
-    a > b ? result = ["a", a] : result = ["b", b]
-    return result;
+const defaultColor = "black";
+const defaultFontFamily = "Verdana";
+
+
+function generateRandomTextStyle() {
+    var inputField = document.getElementById("input-text")
+
+    var fontType = [
+        "Arial",
+        "Helvetica",
+        "Roboto",
+        "Trebushet MS",
+        "Times New Roman"
+    ];
+    var fontColor = [
+        "red",
+        "bisque",
+        "cyan", "orange",
+        "listcyan",
+        "mistyrose",
+        "gray",
+        "darkred"
+    ];
+
+    function getRandomStyle(styleArray) {
+        var index = Math.floor(Math.random() * styleArray.length);
+        return styleArray[index];
+    }
+
+    inputField.style.color = getRandomStyle(fontColor);
+    inputField.style.fontFamily = getRandomStyle(fontType);
+    inputField.style.width = "100%";
 }
 
-console.log(theBiggest(2, 5))
+function resetDefaultInputSyle() {
+    var inputField = document.getElementById("input-text");
+    inputField.style.fontFamily = defaultFontFamily;
+    inputField.style.color = defaultColor;
+}
