bfe78edeb7df8da167acfb12541e06a2db28412c
pavelkuz99 <pavel.kuzmich2014@yandex.com>
2020-10-04 20:34:04 +0300

    wip

--- kr1/script.js
+++ kr1/script.js
@@ -1,30 +1,109 @@
-import crypto from 'crypto';
+function hashCode(s) {
+    return s.split('').reduce((a, b) => (((a << 5) - a) + b.charCodeAt(0)) | 0, 0);
+}
 
-class DeviceDistibutor {
-    constructor(name, country, address, deviceMap) {
+class Company {
+    constructor(name, country, address, phone, product) {
         this.companyName = name;
         this.country = country;
         this.address = address;
-        this.deviceMap = deviceMap;
+        this.phone = phone;
+        if (typeof product != "undefined") {
+            this.addProduct(product);
+        } else {
+            this.products = []
+        }
     }
 
-    /**
-     * @param {any} id
-     */
-    set companyeId(id) {
-        this.companyId = crypto.createHash('sha1').update(id);
+    // method to add id attribute to object
+    setCompanyId() {
+        this.companyId = hashCode(this.companyName);
     }
 
+    // static method to create product dict
+    static createProduct(name, color, price) {
+        return {
+            "name": name,
+            "color": color,
+            "price": price
+        };
+    }
+
+    // add given product to object products array
+    addProduct(product) {
+        if (!this.products) {
+            this.products = [product];
+        } else {
+            this.products.push(product)
+        }
+    }
+
+    // get object information as text
     get infoText() {
-
+        var product;
+        var productsString = "";
+        for (product of this.products) {
+            productsString = productsString + ` ${product["name"]},`;
+        }
+        return `Company "${this.name}" is based in ${this.country}, ${this.address}.<br>` + `Available products:${productsString}`;
     }
 
-    get infoList() {
+    // get object information as list
+    // get infoList() {
 
-    }
-
-    get infoDropdown() {
-
-    }
-
+    // }
 }
+
+var nameField = document.getElementById('name');
+var countryField = document.getElementById('country');
+var addressField = document.getElementById('address');
+var phoneField = document.getElementById('phone');
+var resultBox = document.getElementById('result-box');
+
+var productName = document.getElementById('product-name');
+var productColor = document.getElementById('product-color');
+var productPrice = document.getElementById('product-price');
+
+var addButton = document.getElementById('add-button');
+var updateButton = document.getElementById('update-button');
+var showButton = document.getElementById('show-button');
+var showTypeSelector = document.getElementById('show-type-selector');
+var showCompanySelector = document.getElementById('show-company-selector');
+var shallAllButton = document.getElementById('show-all-button');
+
+
+addButton.addEventListener('click', function() {
+    const defaultStub = "empty";
+
+    if (typeof(Storage) !== "undefined") {
+        name = nameField.value ? nameField.value : defaultStub;
+        country = countryField.value ? countryField.value : defaultStub;
+        address = addressField.value ? addressField.value : defaultStub;
+        phone = phoneField.value ? phoneField.value : defaultStub;
+        if (productPrice.value && productName.value) {
+            product = Company.createProduct(productName.value, productColor.value, productPrice.value);
+        } else {
+            product = "undefined"
+        }
+        var companyInstance = new Company(name, country, address, phone, product);
+        console.log("Print", companyInstance)
+        companyInstance.setCompanyId();
+        localStorage.setItem(companyInstance.companyId, JSON.stringify(companyInstance));
+        var companyOption = document.createElement("option");
+        companyOption.id = companyInstance.companyId;
+        companyOption.innerHTML = companyInstance.companyName;
+        showCompanySelector.appendChild(companyOption)
+    } else {
+        console.log("ERROR: WebStorage not supported");
+    }
+});
+
+showButton.addEventListener('click', function() {
+    resultBox.value = "";
+    var selectedCompany = showCompanySelector.options[showCompanySelector.options.selectedIndex];
+    if (selectedCompany.id != "") {
+        var companyInstance = JSON.parse(localStorage.getItem(selectedCompany.id));
+        console.log("RESULT", companyInstance);
+        console.log(companyInstance.getInfoText());
+    }
+});
